f399a75080c0796cdadab22c02eca9f45ec031f4
Cristobal León Merino Tobar <clmerinotobar@gmail.com>
2023-03-02 00:07:55 -0300

    Sun Solutions v1.6

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const boton = document.querySelector(".opcion_comprar");
 
 boton.addEventListener('click', () => {
-    document.location = '../pages/sinStock.html';
+    document.location = './pages/sinStock.html';
 })
 
 //información y validacion del formulario de empleo va aquí
